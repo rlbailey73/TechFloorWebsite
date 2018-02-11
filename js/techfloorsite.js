897d153bc94bd300cee4e406d4452c57d9d34703
rlbailey73 <36048613+rlbailey73@users.noreply.github.com>
2018-02-11 18:09:06 -0500

    adding nav files and changing nav system

---
+++ js/techfloorsite.js
@@ -0,0 +1,10 @@
+/* ********
+ *Authors: Breanna Greggs and Rebecca Bailey
+ *Purpose: serve as our custom javascript page in order to have fancy pages
+ ******* */
+
+/*
+Function: loadNavBar
+Purpose: be called in order to load the navbar on each html page rather than pasting the nav
+bar code onto each page.
+ */
