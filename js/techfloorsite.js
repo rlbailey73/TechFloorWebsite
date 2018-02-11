5b85cfc9259f4b21e6cda37e1ddec7b669bcdff8
rlbailey73 <36048613+rlbailey73@users.noreply.github.com>
2018-02-11 18:55:08 -0500

    navbar confusion

--- js/techfloorsite.js
+++ js/techfloorsite.js
@@ -8,3 +8,8 @@ Function: loadNavBar
 Purpose: be called in order to load the navbar on each html page rather than pasting the nav
 bar code onto each page.
  */
+function loadNavBar() {
+    $(document).ready(function() {   //$ = Jquery
+        $('#navbar').load('navbar.html');
+    });
+};
