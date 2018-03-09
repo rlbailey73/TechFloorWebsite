24e98fc8d5eb95a2e6c2cc8af1e6a8dbef652016
Bre3annam <b.m.greggs@eagle.clarion.edu>
2018-03-08 22:01:31 -0500

    working news upload pics maybe not

---
+++ php/uploadednews.php
@@ -0,0 +1,33 @@
+<?php
+//This holds the correct directory that we wish to send a file and then cancatonates the
+    //orig file name so that the file will appear as the file it was uploaded as
+$uploadFile = '../news/' . $_FILES['userfile']['name'];
+
+//make sure user uploaded a file
+if($_FILES['userfile']['error']==UPLOAD_ERR_NO_FILE)
+{
+    //informs user a file must be uploaded first
+    $nofile = "No file attached. Attach a file and try again.";
+    echo"$nofile";
+}
+//checks to see if the file is already in the resources and if it is, replaces it
+elseif(file_exists($uploadFile))
+{
+    move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadFile);
+    $replace = "The file was successfully replaced!";
+    echo"$replace";
+}
+//if it doesn't exist we add it
+else if(move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadFile))
+{
+    //if file gets added then alert informs user
+    $successupload = "The file was successfully uploaded!";
+    echo"$successupload";
+}
+//generic default error message
+else {
+    //if the file can't be uploaded
+    echo "File Upload Error\n Debugging info:";
+    print_r($_FILES);
+}
+?>
