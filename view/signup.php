3f381c8915065dc9ec6200ac000ff3c08e2d6317
rlbailey73 <36048613+rlbailey73@users.noreply.github.com>
2018-03-09 02:00:16 -0500

    sing up page and nav changes

--- view/signup.php
+++ view/signup.php
@@ -43,12 +43,19 @@ Purpose: This is the main page people will first see and the one they will be re
             <!-- border across top-->
             <img class = "col-10 offset-1" src = "../images/temp.png" alt = "cool blue techy background"  >
 
-            <div class = "col-6 offset-3"><!-- start News Feed message_box -->
-                <h1 class="message_box_title">Under Construction</h1>
-            </div><!-- end News Feed message_box -->
-        </div><!-- end row -->
-    </div><!--end index-template-->
+        <div class = "col-4 offset-4"><!-- start Suggestions message_box -->
+            <h1 class="message_box_title">Sign Up for Newsletter</h1>
+            <div class = "message_box">
+                <form enctype="multipart/form-data" action="../php/send_email.php" method = "post" class="feed_side_space row">
+                    <input class = "form-control suggestion_item input_length" type = "text" placeholder="Fist Name">
+                    <input class = "form-control suggestion_item input_length" type = "text" placeholder="Last Name">
+                    <input class = "form-control suggestion_item input_length" type = "email" placeholder="email@eagle.clarion.edu">
+                    <input class = "form-control suggestion_item input_length" type = "submit" >
+                </form><!--end the form for suggestions-->
+            </div>
+        </div><!-- end Suggestion message_box -->
 
+    </div><!--end index-template-->
 
 <!-- FOOTER START -->
 <footer>
