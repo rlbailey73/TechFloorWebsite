1828fc1dbe695b7cb0cf822cd848f805e1aa46e0
rlbailey73 <36048613+rlbailey73@users.noreply.github.com>
2018-04-19 19:28:04 -0400

    fixes to member searchpages

--- view/signup.php
+++ view/signup.php
@@ -27,51 +27,6 @@ require '../view/headerinclude.php';
                 </form><!--end the form for suggestions-->
             </div>
         </div><!-- end Suggestion message_box -->
-
-            <!-- Member List View -->
-            <table class="text-white">
-                <thead>
-                <!-- tr = table row -->
-                <tr>
-                    <!-- th = table head -->
-                    <th><h6>First Name</h6></th>
-                    <th><h6>Last Name</h6></th>
-                    <th><h6>Position</h6></th>
-                    <th><h6>Member Since</h6></th>
-                </tr>
-                </thead>
-                <tbody>
-                <!--in order to generate the table data, we would have to go back and forth
-                        between html and php so to make things simpler we just add individual parts of php-->
-                <!-- here we start our php loop
-                it then switches back to html to -->
-                <?php  $i=0; foreach($memList as $row){
-                    $i++;
-                    ?>
-                    <!--the php in this tag will add the class evenRow/oddRow based on our counter in the above tag $i
-                        ***Bonus!! we are using a terinary to do so*** (conditional)? trueCase:falseCase-->
-                    <tr class="<?php echo($i %2 ==0)? 'rowEven':'rowOdd'?>" >
-                        <!--using php we add the array values by using the column names that we
-                            want(CASE SENSITIVE) meaning we could import all info and only get
-                            specific items.    Also, we need the "echo" part so that we see the information
-                            bc html will not read our $row['columnName']. Forgetting the "echo" wont cause a syntax error but the text will be missing-->
-                        <td class="leftText">
-                            <a href="../controller/controller.php?action=ViewMember&MemberID=<?php echo $row['MemberID'] ?>" >
-                                <?php echo $row['FirstName'] ?>
-                            </a>
-                        </td>
-                        <td class="leftText"><?php echo $row['LastName'] ?></td>
-                        <td class="leftText"><?php echo $row['Position'] ?></td>
-                        <td><?php echo toDisplayDate($row['MemberSince']) ?></td>
-                    </tr>
-                    <!--here is where we end our php loop
-                    in between the two tags is the line that gets repeated over and over again-->
-                <?php   }   ?>
-                </tbody>
-                <tfoot>
-
-                </tfoot>
-            </table>
         </div>
     </div><!--end index-template-->
 <?php
