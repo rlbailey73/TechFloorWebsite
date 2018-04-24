16afe2242b4a1b19a34d5157aa094802c87c8f90
Bre3annam <b.m.greggs@eagle.clarion.edu>
2018-04-23 22:56:58 -0400

    model push

--- model/model.php
+++ model/model.php
@@ -4,14 +4,14 @@
     //return our pdo object which represents the db connection
     function getDBConnection()
     {
-        //contains where you wanna run and the db name
-        //$dsn = 'mysql:host=cisprod;dbname=s_bmgreggs_localtechfloor';
-        $dsn = 'mysql:host=localhost;dbname=s_bmgreggs_localtechfloor';
-        //$dsn = 'mysql:host=localhost;dbname=s_rlbailey_techfloordemo'; //access to my database for local hosting purposes
+        //$dsn contains where you wanna run and the db name
+        $dsn = 'mysql:host=localhost;dbname=s_bmgreggs_localtechfloor'; //access to bre's local db and to cisprod
+        //$dsn = 'mysql:host=localhost;dbname=s_rlbailey_techfloordemo'; //access to beckys database for local hosting purposes
         //login credentials
         /** We need to make sure we change this back before submitting $username */
         $username = 's_rlbailey';
         $password = 'techfloor99';
+        //for local testing
         //$username = 'root';
         //$password = '';
 
