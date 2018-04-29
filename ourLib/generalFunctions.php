4b9bccc4b8a935f08361cacbea21f71d730a8ccc
Bre3annam <b.m.greggs@eagle.clarion.edu>
2018-04-29 13:50:30 -0400

    date is fixed

--- ourLib/generalFunctions.php
+++ ourLib/generalFunctions.php
@@ -5,7 +5,7 @@
         //try to convert date from db to normal date
         if($phpDate=strtotime($date))
         {
-            return date('m/d/Y'); //don't do mm/dd/yy it get weird!!!
+            return date('m/d/Y', $phpDate); //don't do mm/dd/yy it get weird!!!
         }
         else //if data was null or not a real date, we return date empty
         {
