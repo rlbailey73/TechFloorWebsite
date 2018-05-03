c370a60478b5cbeeb4ba74a24af9cfe687fbac58
rlbailey73 <36048613+rlbailey73@users.noreply.github.com>
2018-05-03 11:44:46 -0400

    fix to stopslashes

--- ourLib/generalFunctions.php
+++ ourLib/generalFunctions.php
@@ -34,7 +34,7 @@
         if(get_magic_quotes_gpc()){
             function stripslashes_gpc(&$value)
             {
-                $value = stripslashes();
+                $value = stripslashes($value);
             }
             array_walk_recursive($_GET, 'stripslashes_gpc');
             array_walk_recursive($_POST, 'stripslashes_gpc');
